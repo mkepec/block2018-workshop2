37c00ac057c4ab16eabfdef92b55d4c80d5ccffe
Marin Kepec <44492730+mkepec@users.noreply.github.com>
2018-12-14 16:19:25 +0100

    Step 7: Make Migrations for Updates

--- migrations/3_update_metadata.js
+++ migrations/3_update_metadata.js
@@ -7,14 +7,17 @@ module.exports = (deployer, helper, accounts) => {
   deployer.then(async () => {
     try {
       // Deploy Metadata.sol
-      await deployer.deploy(Metadata)
+      await deployer.deploy(Metadata, {replace: true})
       let metadata = await Metadata.deployed()
       console.log(_ + 'Metadata deployed at: ' + metadata.address)
 
       // Deploy Token.sol
-      await deployer.deploy(Token, 'Token Name', 'Token Symbol', metadata.address)
-      let token = await Token.deployed()
+      // await deployer.deploy(Token, 'Token Name', 'Token Symbol', metadata.address)
+      let token = await Token.deployed();
+      await token.updateMetadata(metadata.address)
       console.log(_ + 'Token deployed at: ' + token.address)
+      await token.updateMetadata(metadata.address)
+      console.log(_ + 'Token metadta updated to ' + metadata.address)
 
     } catch (error) {
       console.log(error)
--- test/Token.test.js
+++ test/Token.test.js
@@ -71,4 +71,3 @@ contract('Token', async function(accounts) {
     })
   })
 })
-})
